b90002aa23bba4153de399f8a3264295ac554eb3
domsq <Domsq@hotmail.com>
2021-11-01 15:47:21 +0000

    Create add_task.html file and configure to allow adding of tasks

--- taskmanager/static/js/script.js
+++ taskmanager/static/js/script.js
@@ -1,4 +1,18 @@
 document.addEventListener('DOMContentLoaded', function() {
+    // Side Nav initialisation  
     let sideNav = document.querySelectorAll('.sidenav');
     M.Sidenav.init(sideNav);
-  });
+
+    // Date Picker initialisation
+    let datePicker = document.querySelectorAll('.datepicker');
+    M.Datepicker.init(datePicker, {
+        format: "dd mmmm, yyyy",
+        i18n: {done: "Select"}
+
+    
+    });
+
+    // Select initialisation
+    let selects = document.querySelectorAll('select');
+    M.FormSelect.init(selects);
+});
